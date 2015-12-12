23b32b876caa37a8b2a97615f41f993b87e33489
wise86Android <wise86android@gmail.com>
2015-12-12 22:52:02 +0100

    create a remote file

---
+++ include/gists/gistsRequest.h
@@ -0,0 +1,13 @@
+#ifndef SRC_GISTS_GISTSREQUEST_H_
+#define SRC_GISTS_GISTSREQUEST_H_
+
+namespace webfs {
+namespace gists{
+	bool create(Node *node);
+}
+}
+
+
+
+
+#endif /* SRC_GISTS_GISTSREQUEST_H_ */
